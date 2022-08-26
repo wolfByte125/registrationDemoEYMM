162c7b2187107fff58738abceb9d053b8185e646
Abel Getiso <58817172+wolfByte125@users.noreply.github.com>
2022-08-26 11:34:28 +0300

    initial push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+const handleSubmit = (e) => {
+  e.preventDefault();
+  alert('Registered Successfully!');
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h3 id="title">REGISTRATION</h3>
+        <form action="#" method="post" onSubmit={(e) => handleSubmit(e)}>
+          <label htmlFor="username">
+            Username
+          </label>
+          <input type="text" id="username" name="username" />
+          <label htmlFor="password">
+            Password
+          </label>
+          <input type="password" id="password" name="password" />
+          <button id="registerBtn">Register</button>
+          </form>
+        </div>
     </div>
   );
 }
